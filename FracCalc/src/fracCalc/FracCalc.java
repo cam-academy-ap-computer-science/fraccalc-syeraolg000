f0c02095cb7030bccab6ed5d5ec33c564d62adc1
igor <igor@igor-PC.Home>
2019-12-08 23:05:32 -0800

    not done

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -6,9 +6,9 @@ public class FracCalc {
 
     public static void main(String[] args) 
     {
-    	Scanner userInput = new Scanner (System.in);
+    	Scanner s = new Scanner (System.in);
     	System.out.println("Enter a equation: ");
-    	String input =  userInput.nextLine();
+    	String input =  s.nextLine();
     	System.out.println(produceAnswer(input));
     	
     }
@@ -21,16 +21,16 @@ public class FracCalc {
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input) {
-  
+    	Scanner s = new Scanner (input);
     	
-    	String operand1 = input;
-    	String operator = "";
-    	String operand2 = "";
-    	//System.out.println(operand1.indexOf(" "));
-    	int operand1index = operand1.indexOf("+");
+    	String operand1 = s.next();
+    	String operator = s.next();
+    	String operand2 = s.next();
+    	//System.out.println(operand1.indexOf(" "));....
+    	//int operand1index = operand1.indexOf("+");
     	//System.out.println(operand1.substring(0,operand1index));
-    	System.out.println(operand1.substring(operand1index + 1));
-    	 return operand1;
+    	//System.out.println(operand1.substring(operand1index + 1));
+    	 return operand2;
     	 
     // TODO: Implement this function to produce the solution to the input
         
