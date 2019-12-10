0a7b5430f98b92d8b879a9f8c65be20246ec48d3
syeraolg000 <syeraolg000@CAM-LAB-08.bgsd.k12.wa.us>
2019-12-10 07:49:45 -0800

    check point 2

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -8,8 +8,13 @@ public class FracCalc {
     {
     	Scanner userInput = new Scanner (System.in);
     	System.out.println("Enter a equation: ");
-    	String input =  userInput.nextLine();
+    	String input = userInput.nextLine();
+    	while (!input.contentEquals("quit")) {
     	System.out.println(produceAnswer(input));
+        System.out.println("type quit to stop");
+    	input = userInput.nextLine();	
+    	
+    	}
     	
     }
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -21,14 +26,29 @@ public class FracCalc {
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input) {
-    	int firstSpace = input.indexOf(' ');
-    	//String operand1 = input.substring(0, firstSpace);
-        String operand1.substring(0, 2);
-   
-    	return  operand1;
+    	int firstSpace = input.indexOf(' ');   	
+    	String operand1 = input.substring(0, firstSpace);
+    	input = input.substring(input.indexOf(' ') + 1);
+    	String operator = input.substring(0, input.indexOf(' '));
+    	input = input.substring(input.indexOf(' ') + 1);
+    	String operand2 = input;
+    	return operand2;
+    } 	
+    	findWholeNum();
+    	
+    public static String findWholNum(String s) {
+    	//mixed numbers if statement
+    	// mixed numbers contains underscore
+    	
+    	//fractions if else statement
+    	//fractions contain a slash
+    	
+    	//whole numbers else statement
+    	
+    
     		
     	}
-    	
+    
     	
     	
     		 
